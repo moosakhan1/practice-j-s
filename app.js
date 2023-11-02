c159630a857932a76d01330b426889f20dd948eb
moosakhan1 <144359037+moosakhan1@users.noreply.github.com>
2023-11-02 21:43:38 +0500

    created practice js

---
+++ app.js
@@ -0,0 +1,47 @@
+ let ge =+prompt("enter your age");
+ if(ge<=20 && ge>=10){
+    console.log("Your age lies between 10 and 20");
+ }
+ else {
+    console.log("your age does not lies between 10 and 20 ")
+ }
+let age =+prompt("enter your age");
+switch (age){
+   case age<=20 && age>=10:
+    console.log("Your age lies between 10 and 20");
+        break; 
+        default:
+            console.log("your age does not lies between 10 and 20 ");
+            break;
+}
+
+let a =+prompt("enter your number");
+if(a%2 === 0){
+    if(a%3 === 0)
+    console.log("It is divisible 2 and 3")
+}
+else {
+    console.log("2 divisible na hua")
+}
+
+
+let b =+prompt("enter your number");
+if (b%2 === 0){
+    console.log("It is divisible 2")
+}
+else if (b%3 === 0){
+    console.log("It is divisible 3")
+}
+else{
+    console.log("2 divisible na hua ya 3 divisible na hua")
+}
+
+let ag =prompt("enter your age");
+if(ag>=18 ){
+    console.log("you can drive");
+}
+else{
+    console.log("you cannot drive");
+}
+
+
